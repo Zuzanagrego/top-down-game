2892ed81637ee5234aedc42079c7ca9c3cef45a9
Zuzka Gregorova <93294795+Zuzanagrego@users.noreply.github.com>
2022-09-16 11:07:08 -0500

    Fixed character movement

--- index.js
+++ index.js
@@ -17,11 +17,25 @@ class Sprite {
     this.image = image;
   }
   draw() {
-    context.drawImage(this.image, -740, -650);
+    context.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
 const background = new Sprite({ position: { x: -740, y: -650 }, image: image });
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
 function animate() {
   window.requestAnimationFrame(animate);
@@ -37,22 +51,53 @@ function animate() {
     playerImage.width / 4,
     playerImage.height
   );
+
+  if (keys.w.pressed && lastKey === "w") {
+    background.position.y += 3;
+  } else if (keys.a.pressed && lastKey === "a") {
+    background.position.x += 3;
+  } else if (keys.s.pressed && lastKey === "s") {
+    background.position.y -= 3;
+  } else if (keys.d.pressed && lastKey === "d") {
+    background.position.x -= 3;
+  }
 }
 animate();
-
+let lastKey = "";
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "w":
-      console.log("pressed w");
+      keys.w.pressed = true;
+      lastKey = "w";
       break;
     case "a":
-      console.log("pressed w");
+      keys.a.pressed = true;
+      lastKey = "a";
       break;
     case "s":
-      console.log("pressed w");
+      keys.s.pressed = true;
+      lastKey = "s";
       break;
     case "d":
-      console.log("pressed w");
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
       break;
   }
 });
